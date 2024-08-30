523010653b8820e23e85c26b1ee80d504c0c81e2
Andres Coppola <coppola.andres@gmail.com>
2024-08-30 00:41:52 -0300

    Removes wetherForecastController and adds JsonProperty Annotations for Categoria Class

--- Storey.DAL/Models/Categoria.cs
+++ Storey.DAL/Models/Categoria.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace Storey.DAL.Models
 {
@@ -12,7 +8,10 @@ namespace Storey.DAL.Models
 	{
 		[Key]
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[JsonIgnore]
 		public int CategoriaId { get; set; }
+		
+		[JsonPropertyName("Categoria")]
 		public string Nombre { get; set; }
 		public virtual ICollection<CategoriaItem> Items { get; set; }
 	}
